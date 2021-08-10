ca755419895b2fc53c5ef58a18b1bdc96f727dd6
akivaab <amabramo18@gmail.com>
2021-08-10 16:13:22 +0300

    add doxyfile

--- Ex01.01/PrintChar.h
+++ Ex01.01/PrintChar.h
@@ -2,23 +2,23 @@
 
 /**
  * @file Print Characters
-*/
+ */
 
 /**
  * @brief accepts character from user
  *
  * @return inputted character
-*/
+ */
 char receiveInput();
 
 /**
  * @brief print a given character
  *
  * @param character to print
-*/
+ */
 void printChar(char character);
 
 /**
  * @brief print inputted characters continuously
-*/
+ */
 void printContinuously();
